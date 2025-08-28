eeb80a4c3177a61baf5da533939813d3d3dbb5cd
Asazel <asafajoruro597@gmail.com>
2025-08-28 12:28:41 -0400

    28-08-2025 modificamos el menu creamos un modal usuario y formulario ususrio

--- vistas/modulos/Usuarios.php
+++ vistas/modulos/Usuarios.php
@@ -18,7 +18,71 @@
       <!-- Default box -->
       <div class="box">
         <div class="box-header with-border">
+          
+          <!--Agregar el boton del modal para crear ususario-->
+          <!-- Button trigger modal -->
+          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+            Agregar usuario
+          </button>
           <h3 class="box-title">Title</h3>
+          <!-- Modal -->
+          <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                    <h4 class="modal-title" id="exampleModalLabel">Registro de usuario</h5>
+                </div>
+                <div class="modal-body">
+                  <form role="form">
+              <div class="box-body">
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Nombre</label>
+                  <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Ingrese Nombre">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Nombre Usuario</label>
+                  <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Ingrese Usuario">
+                </div>
+                <div class="form-group">
+                  <label for="">Perfil</label>
+                  <select name="" id="" class="form-control">
+                    <option value="">Tecnico</option>
+                    <option value="">Administrador</option>
+                  </select>
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Password</label>
+                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputFile">Foto</label>
+                  <input type="file" id="exampleInputFile">
+
+                  <p class="help-block">Example block-level help text here.</p>
+                </div>
+                <div class="checkbox">
+                  <label>
+                    <input type="checkbox"> Check me out
+                  </label>
+                </div>
+              </div>
+              <!-- /.box-body -->
+
+              <div class="box-footer">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+            </form>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+              </div>
+            </div>
+          </div>
 
           <div class="box-tools pull-right">
             <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
@@ -42,3 +106,4 @@
     </section>
     <!-- /.content -->
   </div>
+  
